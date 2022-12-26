ee5c6ed0bdcf77cebee95c6c4176cc2bd3a45bed
unknown <sathankothandam@gmail.com>
2022-12-26 07:02:11 -0800

    resume solved

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -4,7 +4,7 @@ import RESUME from "../assets/Sathan_Resume.pdf"
 import { FiDownload } from 'react-icons/fi';
 const Resume = () => {
   return (
-    <Link href={RESUME} download={RESUME}>
+    <Link onClick={() => window.open("https://drive.google.com/file/d/19axX1WjYJE60ovQ2pueKvgZsFORhn_MB/view?usp=share_link")} download href={RESUME} target="_blank">
     {/* <a href="https://drive.google.com/file/d/19axX1WjYJE60ovQ2pueKvgZsFORhn_MB/view?usp=share_link"> */}
     <Button rightIcon={<FiDownload />} bg={"#319795"}>RESUME</Button>
     {/* </a> */}
